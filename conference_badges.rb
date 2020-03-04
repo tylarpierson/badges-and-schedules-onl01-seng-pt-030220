39792052e5d13a2bc111bb1907c277c85bd4b48f
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-04 17:40:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,4 @@
 speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name) 
-  name.each do |speaker|
-    puts "Hello, my name is #{speaker}"
-  end
+  puts "Hello, my name is #{name}"
 end
