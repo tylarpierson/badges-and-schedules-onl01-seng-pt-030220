1d58660e0dd310df0cc1d295712b701698a52abf
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-04 17:53:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,6 +4,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array) 
-  array.collect {|name| "Hello, my name is " + name}
+  array.collect {|name| "Hello, my name is " + name + "."}
 end
 batch_badge_creator(attendees)
