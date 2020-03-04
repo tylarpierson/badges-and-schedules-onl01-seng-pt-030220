b421f92f840ee8bf1cd40ef377d38da52ac8e8ab
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-04 17:46:02 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,8 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees) 
+  new_array = []
   attendees.each do |attendee|
-    return "Hello, my name is #{attendee}"
+    new_array << "Hello, my name is #{attendee}"
   end
 end
